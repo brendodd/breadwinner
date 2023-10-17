432ab5e2b44bf00a05bd61b767adbf471682c801
Brendan Dodd <brendananthonydodd@gmail.com>
2023-10-17 14:18:26 +0200

    added a fade in for the website with js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('in-view')
+    }
+  })
+}, {
+  rootMargin: '0px',
+  threshold: [0, 0.1, 1]
+},
+)
 
+const tags = document.querySelectorAll('figure')
+
+tags.forEach((tag) => {
+  observer.observe(tag)
+})
