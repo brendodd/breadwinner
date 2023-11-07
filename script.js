6b1edcf0da172a7b3f5b36b56481f192c5c992eb
Brendan Dodd <brendananthonydodd@gmail.com>
2023-11-07 11:27:56 +0100

    removed fade when scrolling

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('in-view')
-    }
-  })
-}, {
-  rootMargin: '0px',
-  threshold: [0, 0.1, 1]
-},
-)
 
-const tags = document.querySelectorAll('figure')
-
-tags.forEach((tag) => {
-  observer.observe(tag)
-})
